e2bfa93b14841d12a2de6dbc64732251549de91f
anupong suwanphop <anupong.s64@rsu.ac.th>
2023-03-22 15:32:57 +0700

    food

--- index.js
+++ index.js
@@ -1,39 +1,99 @@
-// Require the necessary modules
-const express = require('express');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
 
-// create a MySQL connection pool
-const pool = mysql.createPool({
-  connectionLimit: 10,
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'test'
+const connection = mysql.createConnection({
+  host: "157.245.59.56",
+  user: "u6401955",
+  password: "6401955",
+  database: "u6401955_8_3",
+  port: 3366,
 });
 
-// Create a new Express app
-const app = express();
-app.use(cors()); // Enable CORS
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+var app = express();
+app.use(cors());
+app.use(express.json());
 
-// Define your API endpoints
-app.get('/users', (req, res) => {
-  pool.query('SELECT * FROM users', (error, results) => {
-    if (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-    } else {
-      res.json(results);
-    }
+app.get("/", function (req, res) {
+  res.json({
+    status: "ok",
+    message: "Api Start",
   });
 });
 
-// start the server
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+app.get("/customers", function (req, res) {
+  connection.query("SELECT * FROM a1_customer", function (err, results) {
+    console.log(results); //แสดงผลที่ console
+    res.json(results); //ตอบกลับ request
+  });
+});
+
+app.get("/products", function (req, res) {
+  connection.query(
+    `SELECT * FROM a1_food`,
+    function (err, results) {
+      res.json(results);
+    }
+  );
+});
+
+app.get("/orders", function (req, res) {
+  connection.query(
+    `SELECT O.order_id, C.Firstname AS customer, P.ชื่อสินค้า AS Product_Name, O.quantity
+    FROM a1_order O
+    LEFT JOIN a1_customer C ON O.customer_id = C.ID
+    LEFT JOIN a1_food P ON O.product_id = P.ID; `,
+    function (err, results) {
+      res.json(results);
+    }
+  );
+});
+
+// เรียงลับดับจากคนที่ซื้อเยอะ => น้อยที่สุด
+app.get("/top_customers", function (req, res) {
+  connection.query(
+    `SELECT 
+    C.FirstName, 
+    SUM(O.quantity*P.ราคา) AS price_sum 
+  FROM a1_customer AS C 
+    INNER JOIN a1_order AS O ON C.ID = O.id 
+    INNER JOIN a1_food AS P ON O.id = P.ID
+  GROUP BY 
+    C.ID
+  ORDER BY 
+    price_sum DESC;`,
+    function (err, results) {
+      res.json(results);
+    }
+  );
+});
+
+// เรียงลับดับจากคนที่ซื้อเยอะ => น้อยที่สุด
+app.get('/top_products', function(req, res){
+  connection.query(
+    `SELECT O.id, P.ชื่อสินค้า, O.quantity, SUM(O.quantity*P.ราคา) as Total_QTY FROM a1_order as O INNER JOIN a1_food as P ON O.id = P.ID GROUP BY O.id, P.ชื่อสินค้า, O.quantity, P.ราคา ORDER BY Total_QTY DESC;`,
+    function (err, results) {
+      res.json(results);
+    }
+  );
+});
+
+
+
+
+app.post('/orders', function(req, res) {
+  const values = req.body
+  console.log(values)
+  connection.query(
+    'INSERT INTO a1_order (order_id, customer_id, product_id, quantity) VALUES ?', [values],
+    function(err, results) {
+      console.log(results) //แสดงผลที่ console
+      res.json(results) //ตอบกลับ request
+    }
+  )
+})
+
+
+app.listen(5000, () => {
+  console.log("Server is started.");
 });
